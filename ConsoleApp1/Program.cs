97772b9520e637b6ed35d708daed35761f1c4673
kantasasakimaru <cfcbr250@gmail.com>
2020-06-07 14:07:27 +0900

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Automation;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumChromeSample
+{
+    class Program
+    {
+        /// <summary>
+        /// 送られてきたURLをChomeで開く
+        /// </summary>
+        /// <param name="args"></param>
+        /// 
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr GetForegroundWindow();
+
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowText", CharSet = CharSet.Auto)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+
+        static void Main(string[] args)
+        {
+
+            String url = "https://www.google.co.jp/";
+            Program program = new Program();
+            // program.openGetUrlForChome(url);
+            // program.GetCurrentUrl();
+            // String currentUrl =  program.GetActiveTabUrl();
+
+            //for(int k = 0; k < 100; k++)
+            // {
+            //    program.getActiveFilePath();
+            //    Thread.Sleep(100);
+            //}
+            // program.getActiveFilePath();
+
+            // program.moveDirectoryOfFile();
+
+        }
+
+        private void openGetUrlForChome(String url)
+        {
+            IWebDriver driver = new ChromeDriver();
+            IWebElement textbox;
+            IWebElement findbuttom;
+
+            //Webページを開く
+            driver.Navigate().GoToUrl(url);
+
+
+            //検索ボックス
+            textbox = driver.FindElement(By.Name("q"));
+            //検索ボックスに検索ワードを入力
+            textbox.SendKeys("Selenium");
+            textbox.Submit();
+            
+            String currentUrl = driver.Url;
+            Console.WriteLine(currentUrl);
+
+        }
+
+        private String GetCurrentUrl()
+        {
+            IWebDriver driver = new ChromeDriver();
+            String currentUrl = driver.Url;
+            Console.WriteLine(currentUrl);
+            return currentUrl;
+        }
+
+        public String GetActiveTabUrl()
+        {
+            AutomationElement.RootElement
+              .FindFirst(TreeScope.Children, new PropertyCondition(AutomationElement.ClassNameProperty, "Chrome_WidgetWin_1"))
+              .SetFocus();
+            SendKeys.SendWait("^l");
+            var elmUrlBar = AutomationElement.FocusedElement;
+            var valuePattern = (ValuePattern)elmUrlBar.GetCurrentPattern(ValuePattern.Pattern);
+           
+            Console.WriteLine(valuePattern.Current.Value);
+
+            return valuePattern.Current.Value;
+        }
+
+        public void getActiveFilePath()
+        {
+            StringBuilder sb = new StringBuilder(65535);//65535に特に意味はない
+            GetWindowText(GetForegroundWindow(), sb, 65535);
+            Console.WriteLine(sb);
+        }
+
+        public void moveDirectoryOfFile(){
+
+            string sourceFile = @"C:\Users\g1723035\Documents\R_D\sample_C#\ConsoleApp1\ConsoleApp1\test.py";
+            string destinationFile = @"C:\Users\g1723035\Documents\test.py";
+
+            // To move a file or folder to a new location:
+            System.IO.File.Move(sourceFile, destinationFile);
+        }
+
+        public void getFile()
+        {
+            Console.WriteLine( System.IO.Directory.GetFiles("test", ".py", System.IO.SearchOption.AllDirectories));
+
+        }
+
+    }
+
+}
---
+++ ConsoleApp1/execWordFile.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.Office.Interop.Word;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace ConsoleApp1
+{
+    class execWordFile
+    {
+
+        public void exec()
+        {
+            try
+            {
+
+                // Word アプリケーションオブジェクトを作成
+                Word.Application word = new Word.Application();
+                // Word の GUI を起動しないようにする
+                word.Visible = false;
+
+                // 新規文書を作成
+                Document document = word.Documents.Add();
+
+                // ヘッダーを編集
+                editHeaderSample(ref document, 10, WdColorIndex.wdPink, "Header Area");
+
+                // フッターを編集
+                editFooterSample(ref document, 10, WdColorIndex.wdBlue, "Footer Area");
+
+                // 見出しを追加
+                addHeadingSample(ref document, "見出し");
+
+                // パラグラフを追加
+                document.Content.Paragraphs.Add();
+
+                // テキストを追加
+                addTextSample(ref document, WdColorIndex.wdGreen, "Hello, ");
+                addTextSample(ref document, WdColorIndex.wdRed, "World");
+
+                // 名前を付けて保存
+                object filename = System.IO.Directory.GetCurrentDirectory() + @"\out.docx";
+                document.SaveAs2(ref filename);
+
+                // 文書を閉じる
+                document.Close();
+                document = null;
+                word.Quit();
+                word = null;
+
+                Console.WriteLine("Document created successfully !");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// 文書のヘッダーを編集する.
+        /// </summary>
+        private static void editHeaderSample(ref Document document, int fontSize, WdColorIndex color, string text)
+        {
+            foreach (Section section in document.Sections)
+            {
+                //Get the header range and add the header details.
+                Range headerRange = section.Headers[WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                headerRange.Fields.Add(headerRange, WdFieldType.wdFieldPage);
+                headerRange.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                headerRange.Font.ColorIndex = color;
+                headerRange.Font.Size = fontSize;
+                headerRange.Text = text;
+            }
+        }
+
+        /// <summary>
+        /// 文書のフッターを編集する.
+        /// </summary>
+        private static void editFooterSample(ref Document document, int fontSize, WdColorIndex color, string text)
+        {
+            foreach (Section wordSection in document.Sections)
+            {
+                //Get the footer range and add the footer details.
+                Range footerRange = wordSection.Footers[WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                footerRange.Font.ColorIndex = color;
+                footerRange.Font.Size = fontSize;
+                footerRange.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                footerRange.Text = text;
+            }
+        }
+
+        /// <summary>
+        /// 文書に見出しを追加する.
+        /// </summary>
+        private static void addHeadingSample(ref Document document, string text)
+        {
+            Paragraph para = document.Content.Paragraphs.Add(System.Reflection.Missing.Value);
+            object styleHeading1 = "見出し 1";
+            para.Range.set_Style(ref styleHeading1);
+            para.Range.Text = text;
+            para.Range.InsertParagraphAfter();
+        }
+
+        /// <summary>
+        /// 文書の末尾位置を取得する.
+        /// </summary>
+        /// <returns></returns>
+        private static int getLastPosition(ref Document document)
+        {
+            return document.Content.End - 1;
+        }
+
+        /// <summary>
+        /// 文書の末尾にテキストを追加する.
+        /// </summary>
+        private static void addTextSample(ref Document document, WdColorIndex color, string text)
+        {
+            int before = getLastPosition(ref document);
+            Range rng = document.Range(document.Content.End - 1, document.Content.End - 1);
+            rng.Text += text;
+            int after = getLastPosition(ref document);
+
+            document.Range(before, after).Font.ColorIndex = color;
+        }
+    }
+}
